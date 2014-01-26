101691d335ffda5019cdfd63d3f126aa768160c7
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-01-27 09:40:26 +1100

    Add two test activities

---
+++ JSEvaluatorTests/src/com/evgenii/jsevaluatortests/EvaluateJsStringActivity.java
@@ -0,0 +1,51 @@
+package com.evgenii.jsevaluatortests;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.evgenii.jsevaluator.JsEvaluator;
+import com.evgenii.jsevaluator.interfaces.JsCallback;
+
+public class EvaluateJsStringActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_evaluate_js_string);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.evaluate_js_string, menu);
+		return true;
+	}
+
+	public void onEvaluateClicked(View view) {
+		final JsEvaluator jsEvaluator = new JsEvaluator(this);
+		final EditText editText = (EditText) findViewById(R.id.editJavaScript);
+		jsEvaluator.evaluate(editText.getText().toString(), new JsCallback() {
+			@Override
+			public void onResult(final String resultValue) {
+				final TextView jsResultTextView = (TextView) findViewById(R.id.textJSResult);
+				jsResultTextView.setText(String.format("Result: %s",
+						resultValue));
+			}
+		});
+
+		// jsEvaluator.callFunction(new JsCallback() {
+		// @Override
+		// public void onResult(final String resultValue) {
+		// final TextView jsResultTextView = (TextView)
+		// findViewById(R.id.textJSResult);
+		// jsResultTextView.setText(String.format("Result: %s",
+		// resultValue));
+		// }
+		// }, "greet", "\"Cow's\" milk'''\"\\\"");
+	}
+
+}
--- JSEvaluatorTests/src/com/evgenii/jsevaluatortests/MainActivity.java
+++ JSEvaluatorTests/src/com/evgenii/jsevaluatortests/MainActivity.java
@@ -1,14 +1,10 @@
 package com.evgenii.jsevaluatortests;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.evgenii.jsevaluator.JsEvaluator;
-import com.evgenii.jsevaluator.interfaces.JsCallback;
 
 public class MainActivity extends Activity {
 	@Override
@@ -24,26 +20,9 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	public void onEvaluateClicked(View view) {
-		final JsEvaluator jsEvaluator = new JsEvaluator(this);
-		final EditText editText = (EditText) findViewById(R.id.editJavaScript);
-		jsEvaluator.evaluate(editText.getText().toString(), new JsCallback() {
-			@Override
-			public void onResult(final String resultValue) {
-				// final TextView jsResultTextView = (TextView)
-				// findViewById(R.id.textJSResult);
-				// jsResultTextView.setText(String.format("Result: %s",
-				// resultValue));
-			}
-		});
-
-		jsEvaluator.callFunction(new JsCallback() {
-			@Override
-			public void onResult(final String resultValue) {
-				final TextView jsResultTextView = (TextView) findViewById(R.id.textJSResult);
-				jsResultTextView.setText(String.format("Result: %s",
-						resultValue));
-			}
-		}, "greet", "\"Cow's\" milk'''\"\\\"");
+	public void onEvaluateJsClicked(View view) {
+		final Intent intent = new Intent(this, EvaluateJsStringActivity.class);
+		startActivity(intent);
 	}
+
 }
