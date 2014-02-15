542e14490213df349a32f9cd42240a5c7b6cf14e
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-02-15 18:05:05 +1100

    Changed target API level to 19. Added status bar button to return to main activity

--- JSEvaluatorTests/src/com/evgenii/jsevaluatortests/CallJsFunctionActivity.java
+++ JSEvaluatorTests/src/com/evgenii/jsevaluatortests/CallJsFunctionActivity.java
@@ -1,8 +1,12 @@
 package com.evgenii.jsevaluatortests;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -28,10 +32,16 @@ public class CallJsFunctionActivity extends Activity {
 		}, "greet", parameterText.getText().toString());
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_call_js_function);
+		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Show the Up button in the action bar.
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
@@ -39,6 +49,16 @@ public class CallJsFunctionActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.call_js_function, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }
--- JSEvaluatorTests/src/com/evgenii/jsevaluatortests/EvaluateJsStringActivity.java
+++ JSEvaluatorTests/src/com/evgenii/jsevaluatortests/EvaluateJsStringActivity.java
@@ -1,8 +1,12 @@
 package com.evgenii.jsevaluatortests;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,10 +16,17 @@ import com.evgenii.jsevaluator.interfaces.JsCallback;
 
 public class EvaluateJsStringActivity extends Activity {
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_evaluate_js_string);
+
+		// Make sure we're running on Honeycomb or higher to use ActionBar APIs
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// Show the Up button in the action bar.
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
@@ -38,4 +49,13 @@ public class EvaluateJsStringActivity extends Activity {
 		});
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
