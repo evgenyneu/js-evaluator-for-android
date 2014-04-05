3b6513d44963277690a88724cd2e252d685e980a
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-04-05 11:23:00 +1100

    Run jQuery test

--- JSEvaluatorTests/src/com/evgenii/jsevaluatortests/RealLibrary.java
+++ JSEvaluatorTests/src/com/evgenii/jsevaluatortests/RealLibrary.java
@@ -1,11 +1,44 @@
 package com.evgenii.jsevaluatortests;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.evgenii.jsevaluator.JsEvaluator;
+
 public class RealLibrary extends Activity {
+	JsEvaluator mJsEvaluator;
+
+	protected void loadAndRunLibrary() {
+		String library_source = null;
+		try {
+			library_source = ReadFile("real_library/jquery-2.1.0.js");
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		mJsEvaluator = new JsEvaluator(this);
+		mJsEvaluator.evaluate(library_source);
+
+		// final String code = "$('<div iiatr=\"26\" />').attr('iiatr')";
+		//
+		// mJsEvaluator.evaluate(code, new JsCallback() {
+		// @Override
+		// public void onResult(final String resultValue) {
+		// final TextView jsResultTextView = (TextView)
+		// findViewById(R.id.realLibraryResultView);
+		// jsResultTextView.setText(String.format("Result: %s", resultValue));
+		// }
+		// });
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -13,6 +46,8 @@ public class RealLibrary extends Activity {
 
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		loadAndRunLibrary();
 	}
 
 	@Override
@@ -32,5 +67,12 @@ public class RealLibrary extends Activity {
 		if (id == R.id.home)
 			return true;
 		return super.onOptionsItemSelected(item);
+	}
+
+	protected String ReadFile(String fileName) throws IOException {
+		final AssetManager am = getAssets();
+		final InputStream inputStream = am.open(fileName);
+
+		return new Scanner(inputStream, "UTF-8").useDelimiter("\\A").next();
 	}
 }
