6e00194d97a6c8fc6ad242f4cad64ecb3bfca6a5
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-04-11 17:29:21 +1000

    Remove unused code

--- JSEvaluatorTests/src/com/evgenii/jsevaluatortests/LineEndingsActivity.java
+++ JSEvaluatorTests/src/com/evgenii/jsevaluatortests/LineEndingsActivity.java
@@ -1,11 +1,6 @@
 package com.evgenii.jsevaluatortests;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
-
 import android.app.Activity;
-import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -14,8 +9,6 @@ import com.evgenii.jsevaluator.interfaces.JsCallback;
 
 public class LineEndingsActivity extends Activity {
 	JsEvaluator mJsEvaluator;
-	private Scanner scanner;
-	String jsCode;
 
 	protected void evaluate() {
 		// Use CR, LF characters and their combinations
@@ -33,26 +26,6 @@ public class LineEndingsActivity extends Activity {
 		});
 	}
 
-	protected void evaluateAndDisplay() {
-		mJsEvaluator.evaluate(jsCode, new JsCallback() {
-			@Override
-			public void onResult(final String resultValue) {
-				final TextView jsResultTextView = (TextView) findViewById(R.id.realLibraryResult);
-				jsResultTextView.setText(String.format("Result:%s", resultValue));
-			}
-		});
-	}
-
-	private String loadJs(String fileName) {
-		try {
-			return ReadFile(fileName);
-		} catch (final IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -64,13 +37,5 @@ public class LineEndingsActivity extends Activity {
 		mJsEvaluator = new JsEvaluator(this);
 
 		evaluate();
-	}
-
-	protected String ReadFile(String fileName) throws IOException {
-		final AssetManager am = getAssets();
-		final InputStream inputStream = am.open(fileName);
-
-		scanner = new Scanner(inputStream, "UTF-8");
-		return scanner.useDelimiter("\\A").next();
 	}
 }
